112c663cc357d09573897b8d0fc378d4da55386b
Ashwin Ramesh <ashramesh1992@gmail.com>
2013-08-12 09:50:55 +1000

    added comments to part 1

--- assignment1.lisp
+++ assignment1.lisp
@@ -6,12 +6,12 @@
 	(if (typep x 'list)
 		(case (list-length x)
 			((1)
-				(cons (find-vars (first x)) ())
+				(cons (find-vars (first x)) ()) ; list size is 1, thus it can be a list or a operator --> s-var, s-and, s-not
 			)
 			((2)
-				(if (and (typep (first x) 'symbol) (typep (first (rest x)) 'symbol))
-					(cons (first (rest x)) ())
-					(append (find-vars (first x)) (find-vars (first (rest x))))
+				(if (and (typep (first x) 'symbol) (typep (first (rest x)) 'symbol)) ; check to see if both items in list are symbols i.e. (s-var a)
+					(cons (first (rest x)) ()) ; return just the variable name in a list
+					(append (find-vars (first x)) (find-vars (first (rest x)))) ; recursively call each half of the query
 				)
 			)
 			(otherwise
